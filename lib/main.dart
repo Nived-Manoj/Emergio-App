3db9e891d5ffcd7a59b38d03781dd9dfb80b375c
Nived-Manoj <nivedmanoj001@gmail.com>
2024-04-01 06:52:36 +0530

    initial comitt

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,17 @@
 import 'package:emergio_app/view/splash_screen.dart';
+import 'package:firebase_core/firebase_core.dart';
 
 import 'package:flutter/material.dart';
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+      options: FirebaseOptions(
+          apiKey: "AIzaSyAUU1J7kz5Bl_CQ3X7vkwYDpqpmPRIZX9U",
+          appId: "1:701544939176:android:9f0207aef1f0539cfb8c9d",
+          messagingSenderId: "",
+          projectId: "emergio-2f3ae"));
+
   runApp(const MyApp());
 }
 
